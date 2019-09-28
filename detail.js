b5d3f559cd4f8a0e0ab302f5602f3484446191fb
hg_jeon <jeonhgfm101@gmail.com>
2019-09-28 15:35:40 +0900

    Updates

--- detail.js
+++ detail.js
@@ -18,16 +18,28 @@ window.onload = async () => {
 			}
 		} = await axios.get(`https://yts.lt/api/v2/movie_details.json?movie_id=${id}`)
 		console.log(data);
-		const title = document.createElement("p");
+		const title = document.createElement("h1");
 		const img = document.createElement("img");
 		const desc = document.createElement("p");
+		const img_div = document.createElement("div");
+		const desc_div = document.createElement("div");
+		const card = document.createElement("div");
 		title.innerText = data.title_long;
 		img.src = data.medium_cover_image;
+		img_div.append(img)
+		img_div.classList.add("detail_img_div")
 		desc.innerText = data.description_full;
-		const card = document.createElement("p");
-		card.append(img, title, desc);
+		desc.classList.add("desc")
+		desc_div.classList.add("detail_desc_div")
+		desc_div.append(title, desc)
+		card.append(img_div, desc_div);
+		card.classList.add('detail_card','container')
 		app.removeChild(loading);
 		app.appendChild(card);
+		const link = document.createElement("a");
+		link.innerText = "영화 리스트로 이동하기";
+		desc_div.appendChild(link)
+		link.setAttribute("href","index")
 	};
 
 	await main();
